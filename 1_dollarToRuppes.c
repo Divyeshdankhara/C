8b27188fe462caf5dd474ed52ad2a5c738eac9d7
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2023-12-21 17:05:05 +0530

    21-12-2023

---
+++ 1_dollarToRuppes.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main()
+{
+    int dolar = 5;
+    int ruppes = 82;
+
+    int total = dolar * ruppes;
+    printf("Dollar to ruppes convert :- %d",total);
+    return 0;
+}

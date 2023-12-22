d3683a4dba381227709f90bdc31106a040c40e89
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2023-12-22 19:59:34 +0530

    22-12-2023

---
+++ 2_FeatToInches.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int feet=5;
+    int inches=12;
+    int total=0;
+    total=feet*inches;
+
+    printf("%d Feet to Inches :-> %d",feet,total);
+
+    return 0;
+}
